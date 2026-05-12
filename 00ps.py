c09c055ee01097189047e676e5470fed2e4379fe
seerabharath123 <seerabharath123@gmail.com>
2026-05-12 15:53:32 +0530

    Update 00ps.py
    oops

--- 00ps.py
+++ 00ps.py
@@ -1,1 +1,49 @@
+class Employee():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def DisplayDetails(self):
+        print(self.name)
+        print(self.age)
+
+EE=Employee("SARA",30)
+print(EE.name)
+print(EE.age)
+EE.DisplayDetails()
+
+
+class DataBinding():
+    def __init__(self):
+        self._x="It is Protected Access"
+        print(self._x)
+
+DD=DataBinding()
+print(DD._x)
+
+class Banking():
+    def PublicMethod(self):
+        print("Public Method")
+    def _ProtectMethod(self):
+        print("Protect Method")
+    def __PrivateMethod(self):
+        print("Private Method")
+
+#CreatingInstance
+BB=Banking()
+BB.PublicMethod()
+BB._ProtectMethod()
+BB._Banking__PrivateMethod()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
