98d16e54c99a66ec7a0e875e07288bf03a7ac609
seerabharath123 <seerabharath123@gmail.com>
2026-05-19 22:45:53 +0530

    Update 00ps.py
    oops

--- 00ps.py
+++ 00ps.py
@@ -34,10 +34,45 @@ BB.PublicMethod()
 BB._ProtectMethod()
 BB._Banking__PrivateMethod()
 
+class
+class Account:
+"""This is docstring, explains brief about the class"""
+	pass
+sb_account=Account()
+current_account=Account()
+class MyBank_SBI():
+           pass
+class MyBank_AB:
+          pass
+class MyBank_HDFC(object):
+          pass
+#SS is an Object or Instance
+SS=MyBank_SBI()
+print(SS)
+#AA is an Object or Instance
+AA=MyBank_AB()
+print(AA)
+#HH is an Object or Instance
+HH=MyBank_HDFC()
+print(HH)
 
+obj
+Object1: Human
+Identity: Midhun
+Properties: Color, Height, Weight
+Functionalities: walk () , see() , run() ....
 
-
-
+method
+class Banking():
+#It is a Method of Member
+#Class inside all are members
+    def MyCustomer():
+        print("Welcome to Banking")
+        return()
+#Driver Code
+BB=Banking()
+BB.MyCustomer()
+#TypeError: MyCustomer() takes 0 positional arguments but 1 was given
 
 
 
